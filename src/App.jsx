eb65def922dbf862a25d9cdcccd2b0ad3ee04057
Eben Ofere <ebenezerofere@gmail.com>
2023-11-22 08:00:41 -0500

    setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Box, Text} from "@chakra-ui/react";
+
+function App() {
+
+  return (
+    <>
+      <Box>
+        <Text>I love Canada</Text>
+      </Box>
+      
+    </>
+  )
+}
+
+export default App
